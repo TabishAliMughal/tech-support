be525eec657332be4612d258334de7685e583712
Tabish <tabishalimughal@gmail.com>
2023-01-04 09:58:49 +0500

    abc

---
+++ migrations/2022_12_30_071143_create_chat_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chat_users', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id');
+            $table->string('name');
+            $table->string('email');
+            $table->text('role');
+            $table->bigInteger('contact');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chat_users');
+    }
+};
